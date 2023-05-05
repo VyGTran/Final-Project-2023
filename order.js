f67eabcea52caf24d7d5d3631adeb9770cd36ae1
CTC CCP Mac 1 <ctcccpmac1@ctcs-air.cape.k12.mo.us>
2023-05-05 10:42:51 -0500

    Working on JS

--- order.js
+++ order.js
@@ -1,33 +1,41 @@
 "use strict";
 
-window.addEventListener("load",function () {
-   calcCart();
-   var cart = document.forms.cart;
-   cart.elements.modelQty.onchange = calcCart;
+window.addEventListener("load", function() {
+    var orderForm = document.forms.orderForm;
+    orderForm.elements.shoe.focus();
 
-   var shippingOptions = document.querySelectorAll('input[name="shipping"]');
-   for (var i = 0;i <= shippingOptions.length; i++) {
-       shippingOptions[i].onclick = calcCart;
-   }
+    calcOrder();
 
+    orderForm.elements.shoe.onchange = calcOrder;
+    orderForm.elements.insurance.onchange = calcOrder;
+
+    var shippingType = document.querySelectorAll ('input[name="shippingType"]');
+    for (var i = 0; i < shippingType.length; i++) {
+        shippingType[i].onclick = calcOrder;
+    }
 });
 
-function calcCart() {
-   var cart = document.forms.cart;
-   var machineCost = cart.elements.modelCost.value;
-   var qIndex = cart.elements.modelQty.selectedIndex;
-   var qty = cart.elements.modelQty[qIndex].value;
+function calcOrder() {
+    var orderForm = document.forms.orderForm;
 
-   var orderCost = machineCost * qty;
-   cart.elements.orderCost.value = formatUSCurrency(orderCost);
+    var mIndex = orderForm.elements.shoe.selectedIndex;
+    var mCost = orderForm.elements.shoe.options[mIndex].value;
+    var qIndex = orderForm.elements.insurance.selectedIndex;
+    var insurance = orderForm.elements.insurance[qIndex].value;
 
-   var shipCost = document.querySelector('input[name="shipping"]:checked').value*qty;
-   cart.elements.shippingCost.value = formatNumber(shipCost);
+    var initialCost = mCost;
+    orderForm.elements.initialCost.value = formatNumber(initialCost)
 
-   cart.elements.subTotal.value = formatNumber(orderCost+shipCost, 2);
+    var iCost = insurance * 20;
+    orderForm.elements.iCost.value = formatUSCurrency(iCost, 2);
 
-   cart.elements.cartTotal.value = formatUSCurrency(orderCost+shipCost);
+    var sCost = document.querySelector('input[name="shippingType"]:checked').value;
+    orderForm.elements.sCost.value = formatUSCurrency(sCost, 2);
 
+    var totalCost = mCost + iCost + sCost;
+    orderForm.elements.totalCost.value = formatUSCurrency(totalCost), 2;
+
+    orderForm.elements.sh
 }
 
 function formatNumber(val, decimals) {
